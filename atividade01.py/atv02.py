32855a319eeb90ea7e251d17189ebbf96a6c836c
CarlosVitorAmorim <136392761+CarlosVitorAmorim@users.noreply.github.com>
2023-06-14 20:29:14 -0300

    atv Primeira Questão

--- atividade01.py/atv02.py
+++ atividade01.py/atv02.py
@@ -1,1 +1,11 @@
 print('rapaz isso ta uma loucura!!')
+def função():
+  soma= a + b
+  sub = a - b
+  mul = a * b
+  divi = a / b
+  print(soma)
+  print(sub)
+  print(mul)
+  print(divi)
+função(2,3)
